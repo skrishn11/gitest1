1615528072f66a9d5112e83dc096b21755201899
shankar <shkrishnamoorthy@pivotal.io>
2019-05-16 14:55:54 -0400

    Initial commit

---
+++ src/main/java/com/demo/simpleweb/Gittest1Application.java
@@ -0,0 +1,13 @@
+package com.demo.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gittest1Application.class, args);
+	}
+
+}
