3ba74ea9cc0d2efed685005d284b594949b34d45
skrishn11 <49248813+skrishn11@users.noreply.github.com>
2019-05-17 09:45:18 -0400

    Update Gittest1Application.java

--- src/main/java/com/demo/simpleweb/Gittest1Application.java
+++ src/main/java/com/demo/simpleweb/Gittest1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Gittest1Application {
 
 	public static void main(String[] args) {
-		System.out.println("***");
+		System.out.println("webhook test");
 		SpringApplication.run(Gittest1Application.class, args);
 	}
 
